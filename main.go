7a4218f4982d974c95394f8bb27d053a154f7adb
Sina Chaichi Maleki <sina.chaichi2020@gmail.com>
2026-04-25 17:14:09 +0200

    Add viper to handle the environment variables as config

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "database/sql"
-    "log"
+	"database/sql"
+	"log"
 
-    _ "github.com/lib/pq"
-    "github.com/sinachaichi/gault/api"
-    db "github.com/sinachaichi/gault/db/sqlc"
+	_ "github.com/lib/pq"
+	"github.com/sinachaichi/gault/api"
+	db "github.com/sinachaichi/gault/db/sqlc"
+	"github.com/sinachaichi/gault/util"
 )
 
-const (
-    dbDriver      = "postgres"
-    dbSource      = "postgresql://root:secret@localhost:25432/bank_db?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
 
 func main() {
-    conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+    if err != nil {
+        log.Fatal("cannot load config:", err)
+    }
+    conn, err := sql.Open(config.DBDriver, config.DBSource)
     if err != nil {
         log.Fatal("Cannot connect to db:", err)
     }
@@ -24,7 +24,7 @@ func main() {
     store := db.NewStore(conn)
     server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
     if err != nil {
         log.Fatal("Cannot start server:", err)
     }
